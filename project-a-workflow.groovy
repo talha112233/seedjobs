ba0745a881c7b5e9dfe550fad1650c8c9b0e552b
talha112233 <108019471+talha112233@users.noreply.github.com>
2024-03-14 12:09:18 +0500

    Update project-a-workflow.groovy

--- project-a-workflow.groovy
+++ project-a-workflow.groovy
@@ -21,3 +21,22 @@ pipeline {
         }
     }
 }
+
+
+
+//
+pipelineJob('example') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/talha112233/seedjobs.git')
+                    }
+                    branches('*/master') // Optionally specify the branch to checkout
+                }
+            }
+            scriptPath('project-a-workflow.groovy') // Specify the path to your script
+        }
+    }
+}
