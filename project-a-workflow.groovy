643b2f956995feaf17cefeb80ba999cee9babf53
talha112233 <108019471+talha112233@users.noreply.github.com>
2024-03-14 12:39:27 +0500

    Update project-a-workflow.groovy

--- project-a-workflow.groovy
+++ project-a-workflow.groovy
@@ -21,3 +21,29 @@ pipeline {
         }
     }
 }
+
+
+
+pipeline {
+    agent any
+    stages {
+        stage('Build nrere') {
+            steps {
+                echo 'Building project A...'
+                // Add your build commands here
+            }
+        }
+        stage('Testjkj') {
+            steps {
+                echo 'Testing project A...'
+                // Add your test commands here
+            }
+        }
+        stage('Deployklj') {
+            steps {
+                echo 'Deploying project A...'
+                // Add your deployment commands here
+            }
+        }
+    }
+}
