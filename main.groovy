2190a802ac1825845133b9bc76fb143e9ad1ea9b
talha112233 <108019471+talha112233@users.noreply.github.com>
2024-03-15 10:37:22 +0500

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -1,21 +1,31 @@
-pipelineJobNames = ['a', 'b',]
-pipelineJobNames.each { jobName ->
-    pipelineJob(jobName) {
-      //triggers {
-            //cron('* * * * *')
-        //}
-      definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/talha112233/seedjobs.git')
-                    }
-                    branches('*/master') // Optionally specify the branch to checkout
-                }
+pipelineJob('example') {
+    definition {
+        cps {
+            script('''
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building project A...'
+                // Add your build commands here
             }
-          scriptPath("${jobName}.groovy") // Specify the path to your script
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing project A...'
+                // Add your test commands here
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying project A...'
+                // Add your deployment commands here
+            }
         }
     }
-  }
+}
+''')
+        }
+    }
 }
